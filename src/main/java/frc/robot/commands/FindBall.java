93d982cae64e7139865af91d41ff9787c5f52696
AndroidB7 <97636818+AndroidB7@users.noreply.github.com>
2022-02-10 16:58:11 -0600

    git push

--- src/main/java/frc/robot/commands/FindBall.java
+++ src/main/java/frc/robot/commands/FindBall.java
@@ -1,12 +1,24 @@
 package frc.robot.commands;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class FindBall {
+    NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+    NetworkTableEntry tx = table.getEntry("tx");
+    NetworkTableEntry ty = table.getEntry("ty");
     public void intialize() {
 
     }
     
     public void execute() {
-    
+        if(tx.getDouble(0.0) > 0) {
+            //turn right
+        } else if(tx.getDouble(0.0) < 0) {
+            //turn left
+        } else {
+            //stop
+        }
     }
     public void end(boolean interrupted) {
     
