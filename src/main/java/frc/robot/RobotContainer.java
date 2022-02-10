386d6d4aa5faa711315d3b8abc66a480ed02fe6e
NotYahir <Yahirhernandez304@gmail.com>
2022-02-10 16:35:37 -0600

    DriveCommand updated and mapped
    Set up the connection between the DriveCommand command, DriveTrain subsystem, and the actual initialization of the RobotContainer!

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,16 +6,14 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.commands.ArcadeDrive;
-import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.commands.DriveCommand;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Subsystem;
+//import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.subsystems.DriveTrain;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import edu.wpi.first.wpilibj.*;
-import edu.wpi.first.wpilibj2.command.RunCommand;
+//import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+//import edu.wpi.first.wpilibj.*;
+//import edu.wpi.first.wpilibj2.command.RunCommand;
 
 
 /**
@@ -26,9 +24,6 @@ import edu.wpi.first.wpilibj2.command.RunCommand;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
 
   WPI_VictorSPX m_left1 = new WPI_VictorSPX(Constants.motor_left1);
   WPI_VictorSPX m_left2 = new WPI_VictorSPX(Constants.motor_left2);
@@ -43,7 +38,7 @@ public class RobotContainer {
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the button bindings
-
+    getDriveCommand();
     configureButtonBindings();
   }
 
@@ -65,11 +60,11 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
-    return m_autoCommand;
+    return null;
   }
   
-  public Command getArcadeDriveCommand() {
-    return new ArcadeDrive(driveTrain,control);
+  public Command getDriveCommand() {
+    return new DriveCommand(driveTrain,control);
   }
 
 
--- src/main/java/frc/robot/commands/DriveCommand.java
+++ src/main/java/frc/robot/commands/DriveCommand.java
@@ -4,25 +4,24 @@ import frc.robot.subsystems.DriveTrain;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-public class ArcadeDrive extends CommandBase {
+public class DriveCommand extends CommandBase {
     private DriveTrain driveTrain;
-    private XboxController control = new XboxController(0);
+    private XboxController control;
 
-    public ArcadeDrive(DriveTrain drivetrain, XboxController controller) {
+    public DriveCommand(DriveTrain drivetrain, XboxController controller) {
         driveTrain = drivetrain;
         control = controller;
         addRequirements(drivetrain);
     }
     
-    
-   
+
     //Hello World
     public void initialize() {
 
     }
 
     public void execute() {
-        driveTrain.manualDrive(control, 2, 3, false);
+        driveTrain.manualDrive(control, 0.5, 0.5, false);
     }
     public void end(boolean interrupted) {
 
