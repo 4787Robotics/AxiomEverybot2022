65a09e0e68101b173f7ac8b15a7378eb6165f9ac
AndroidB7 <97636818+AndroidB7@users.noreply.github.com>
2022-02-25 11:19:28 -0600

    Intake commands/subsystem

--- src/main/java/frc/robot/subsystems/IntakeArm.java
+++ src/main/java/frc/robot/subsystems/IntakeArm.java
@@ -6,16 +6,35 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.math.controller.PIDController;
 
 public class IntakeArm extends SubsystemBase {
   private MotorController armMotor;
+  private Encoder armEncoder;
   private MotorController intakeMotor;
+  private PIDController armPID;
 
   /** Creates a new IntakeArm. */
-  public IntakeArm(MotorController arm, MotorController intake) {
+  public IntakeArm(MotorController arm, MotorController intake, Encoder encoder) {
     armMotor = arm;
     intakeMotor = intake;
+    armEncoder = encoder;
+    armEncoder.reset();
   }
+
+  public void setArmSpeed(double speed) {
+    armMotor.set(speed);
+  }
+
+  public void setIntakeSpeed(double speed) {
+    intakeMotor.set(speed);
+  }
+
+  public double getPosition() {
+    return armEncoder.getDistance();
+  }
+
 
   @Override
   public void periodic() {
