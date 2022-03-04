de7ef99acee488d1e8ea6a4a449e25e6ba2530ea
4787 Robotics <4787software@gmail.com>
2022-03-04 15:45:08 -0600

    Pt. 2

--- src/main/java/frc/robot/commands/DriveCommand.java
+++ src/main/java/frc/robot/commands/DriveCommand.java
@@ -22,7 +22,6 @@ public class DriveCommand extends CommandBase {
     @Override
     public void execute() {
         driveTrain.manualDrive(throttle.getAsDouble(), steer.getAsDouble(), 0.7, 0.7, true);
-        driveTrain.chooseName();
     }
 
     @Override
